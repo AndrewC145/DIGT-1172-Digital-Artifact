573ce28c1ab6df4090d9ffa106ed9bf5ea01bee4
Andrew Cao <andrewcao618@gmail.com>
2024-11-07 20:40:50 -0500

    Added Loading Animation

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+  let reveals = document.querySelectorAll('.reveal');
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let revealTop = reveals[i].getBoundingClientRect().top;
+    const revealPoint = 150;
+
+    if (revealTop < windowHeight - revealPoint && !reveals[i].classList.contains('revealed')) {
+      reveals[i].classList.add('active');
+      reveals[i].classList.add('revealed'); // Mark as revealed
+    }
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  reveal();
+});
+
+
+
 const imageSlider = document.querySelector('.image-slider').cloneNode(true);
 document.querySelector('.slider-container').appendChild(imageSlider);
